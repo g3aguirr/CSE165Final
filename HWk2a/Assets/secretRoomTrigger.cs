a97209defc93dd3ee67158721dd12caab41c6360
orangeisnotred <yifanxuchn@outlook.com>
2019-03-09 03:23:21 -0800

    secretRoomPuzzle

---
+++ HWk2a/Assets/secretRoomTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secretRoomTrigger : MonoBehaviour
+{
+	Collider m_ObjectCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ObjectCollider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (butterfly.RED == true && fish.BLUE == true && skull.GREEN == true)
+		{
+			m_ObjectCollider.isTrigger = true;
+			// Debug.Log("ALLRIGHT: "+GameObject.GetComponent<BoxCollider>().isTrigger);
+		}
+    }
+}
